437d9e41bb9175e5b3a99d9eae7af8946481c7b3
Darius Spruogis <darius.spruogis@gmail.com>
2024-05-18 08:14:37 +0300

    Clean up

--- chat_app_client/src/main/java/app/chat_app_client/ClientLauncher.java
+++ chat_app_client/src/main/java/app/chat_app_client/ClientLauncher.java
@@ -1,5 +1,8 @@
 package app.chat_app_client;
 
+/**
+ * @author Darius Spruogis
+ */
 public class ClientLauncher {
     public static void main(String[] args) {
         ServerConnection.getInstance().start();
--- chat_app_server/src/main/java/app/chat_app_server/ServerLauncher.java
+++ chat_app_server/src/main/java/app/chat_app_server/ServerLauncher.java
@@ -2,6 +2,10 @@ package app.chat_app_server;
 
 import app.chat_app_server.fileIO.DataSaver;
 
+
+/**
+ * @author Darius Spruogis
+ */
 public class ServerLauncher {
     public static void main(String[] args) {
         DataSaver dataSaver = new DataSaver();
